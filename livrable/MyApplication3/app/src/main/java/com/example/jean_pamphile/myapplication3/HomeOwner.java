8aa6d106680b22465b4428add279e4cf780e1872
karljr11 <kdupu059@uottawa.ca>
2018-11-20 19:18:23 -0500

    remove files

--- livrable/MyApplication3/app/src/main/java/com/example/jean_pamphile/myapplication3/HomeOwner.java
+++
@@ -1,9 +0,0 @@
-package com.example.jean_pamphile.myapplication3;
-
-public class HomeOwner extends Account {
-private String type = "Home Owner";
-
-    public HomeOwner(String username, String password, String type) {
-        super(username, password);
-    }
-}
--- livrable2/MyApplication3/app/src/main/java/com/example/jean_pamphile/myapplication3/Provider.java
+++
@@ -1,8 +0,0 @@
-package com.example.jean_pamphile.myapplication3;
-
-public class Provider extends Account {
-
-    public Provider(String username, String password) {
-        super(username, password);
-    }
-}
